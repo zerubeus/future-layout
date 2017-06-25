0801ae9103a80bd36136f1fcd2c782786ab55aa4
zerubeus <zerubeus@gmail.com>
2017-06-25 23:46:07 +0200

    fix immutable usage

--- src/core/layout/reducer.js
+++ src/core/layout/reducer.js
@@ -2,15 +2,13 @@ import { Record } from 'immutable';
 import { layoutActions } from './actions';
 
 export const layoutState = new Record({
-  layout: 'There is no profile yet'
+  layout: {} 
 });
 
 export function layoutReducer(state = new layoutState(), {payload, type}) {
   switch (type) {
     case layoutActions.LAYOUT_MODEL_RECEIVED:
-      return state.merge({
-        layout: payload
-      });  
+      return state.set('layout', payload);  
     default:
       return state;
   }
